e942c1e1adbb911d2a874e083ade34666f9a15ae
yliu <yliu_625@163.com>
2022-06-24 11:11:47 -0500

    6-24-22

--- SQL/easy_level.sql
+++ SQL/easy_level.sql
@@ -98,4 +98,120 @@ ORDER BY id
 *####No.176 (Amazon, Infosys, Apple) select twice for none record
 SELECT (SELECT DISTINCT salary FROM Employee
 ORDER BY salary DESC
-LIMIT 1, OFFSET 1) AS SecondHighestSalary 
+LIMIT 1, OFFSET 1) AS SecondHighestSalary
+
+####No.175 (apple)
+SELECT firstName, lastName, city, state FROM Person
+LEFT JOIN Address On Person.personId=Addrdss.personId
+
+####No.1581 (nerdwallet)
+SELECT customer_id, COUNT(customer_id) AS count_no_trans FROM Visits
+LEFT JOIN Transactions On Visits.visit_id=Transactions.visit_id
+WHERE amount IS NULL
+GROUP BY customer_id
+
+####No.1148 (LinkedIn)
+SELECT DISTINCT author_id AS id FROM Views
+WHERE author_id=viewer_id
+ORDER BY id
+
+*####No.197 (Adobe) (date difference)
+SELECT Weather.id AS id FROM Weather JOIN Weather w
+ON datediff(Weather.recordDate, w.recordDate) =1 
+WHERE Weather.temperature > w.temperature
+
+***####No.607 (Amazon) when there is a null value, try not in instead of equal 
+SELECT SalesPerson.name FROM SalesPerson 
+WHERE SalesPerson.sales_id NOT IN
+(SELECT Orders.sales_id FROM Orders LEFT JOIN Company USING (com_id) 
+WHERE name LIKE 'RED')
+
+####No.1141 (Facebook)
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users FROM Activity
+WHERE activity_date <= '2019-07-27' AND activity_date > '2019-06-27'
+GROUP BY activity_date;
+
+####No.1693
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id,make_name
+
+####No.1729 (Tesla)
+SELECT user_id, COUNT(follower_id) AS followers_count FROM Followers
+GROUP BY user_id
+ORDER BY user_id
+
+####No.586 (Google)
+SELECT customer_number FROM (SELECT customer_number, COUNT(order_number) AS order_counts FROM Orders
+GROUP BY customer_number
+ORDER BY order_counts DESC
+LIMIT 1) AS Tmp
+
+SELECT customer_number FROM (SELECT COUNT(order_number) AS total, customer_number FROM Orders
+GROUP BY customer_number) AS Summary
+ORDER BY total DESC
+LIMIT 1;
+
+####No.511 (Amazon)
+SELECT player_id, min(event_data) AS first_login FROM Activity
+GROUP BY player_id
+
+####No.1890 max/min also works for time
+select user_id, max(time_stamp) AS last_stamp FROM logins
+WHERE time_stamp >= '2020-01-01' AND time_stamp <= '2020-12-31'
+GROUP BY user_id
+
+####No.1741 (Amazon)
+SELECT event_day AS day, emp_id, sum(out_time-in_time) AS total_time
+FROM Employees
+GROUP BY day, emp_id
+
+*####No.1383 (robinhood)
+SELECT stock_name, SUM(CASE
+WHEN operation='Buy' THEN -price
+WHEN operation='Sell' THEN +price
+END) AS capital_gain_loss FROM Stocks
+GROUP BY stock_name
+
+*####No.1407 (point72) different people might have same name, group by users.id
+SELECT name, IFNULL(SUM(distance),0) AS travelled_distance FROM Users
+LEFT JOIN Rides ON Users.id=Rides.user_id
+GROUP BY Users.id
+ORDER BY travelled_distance DESC, name
+
+*####No.1158 poshmark
+SELECT Users.user_id AS buyer_id, join_date, IFNULL(COUNT(order_id),0) AS orders_in_2019
+FROM Users LEFT JOIN (SELECT * FROM Orders 
+WHERE order_date >='2019-01-01' AND order_date <='2019-12-31') AS Order2019
+ON Users.user_id=Order2019.buyer_id
+GROUP BY user_id
+
+SELECT user_id AS buyer_id,join_date, IFNULL(orders_in_2019,0) AS orders_in_2019  FROM Users LEFT JOIN
+(SELECT buyer_id, COUNT(order_id) AS orders_in_2019 FROM Orders
+WHERE order_date>='2019-01-01' AND order_date<='2019-12-31'
+GROUP BY buyer_id) AS Ordercount ON Users.user_id=Ordercount.buyer_id
+
+####No.182 if first group by, then having instead of where
+SLEECT email AS Email FROM Person
+GROUP BY email HAVING COUNT(email)>1
+
+*####No.1050 (AMAZON)
+SELECT actor_id, director_id FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp)>=3
+
+####No.1587 ### using is faster
+SELECT NAME, sum(amount) AS balance FROM Users
+LEFT JOIN Transactions ON Users.account=Transactions.account
+GROUP BY name HAVING balance>10000
+
+SELECT name, SUM(amount) AS balance FROM Users 
+LEFT JOIN Transactions USING (account)
+GROUP BY name
+HAVING balance > 10000
+
+####No.1084 (Amazon)
+SELECT product_id, product_name FROM Product
+LEFT JOIN Sales USING (product_id)
+GROUP BY product_id
+HAVING min(sale_date)>='2019-01-01' AND max(sale_date)<='2019-03-31'
