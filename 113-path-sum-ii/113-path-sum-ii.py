1c7605c003d84aa2ebbb60595bd53d7f91645db3
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-06-25 14:05:35 -0500

    Time: 725 ms (5.03%), Space: 16.2 MB (50.13%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -11,23 +11,21 @@ class Solution(object):
         :type targetSum: int
         :rtype: List[List[int]]
         """
-    
         def traversal(cur_node,remain):
+            path.append(cur_node.val)
+            print path
             if not cur_node.left and not cur_node.right:
                 if remain==0:
                     result.append(path[:])
             if cur_node.left:
-                path.append(cur_node.left.val)
                 traversal(cur_node.left,remain-cur_node.left.val)
                 path.pop()
             if cur_node.right:
-                path.append(cur_node.right.val)
                 traversal(cur_node.right,remain-cur_node.right.val)
                 path.pop()
         result,path=[],[]
         if not root:
             return result
-        path.append(root.val)
         traversal(root,targetSum-root.val)
         return result            
                     
