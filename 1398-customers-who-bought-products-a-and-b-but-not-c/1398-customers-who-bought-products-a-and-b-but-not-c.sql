4f65e81101161f4a6b00e843d4f5ed2b1794327a
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-06-27 10:39:27 -0500

    Time: 1106 ms (13.58%), Space: 0B (100.00%) - LeetHub

---
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT customer_id, customer_name FROM Customers LEFT JOIN Orders USING(customer_id)
+GROUP BY customer_id
+HAVING sum(product_name='A') >0 AND 
+    sum(product_name='B') >0 AND
+    sum(product_name='C') =0
