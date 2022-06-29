b86395e8636c09842b56adec3acf4619c72eb76a
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-06-29 17:34:11 -0500

    Time: 95 ms (86.89%), Space: 14.8 MB (84.03%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        fin=[]
+        path=[]
+        def backtracking(n,k,index):
+            if len(path)==k:
+                fin.append(path[:])
+                return
+            for i in range(index,n-(k-len(path))+2):
+                path.append(i)
+                backtracking(n,k,i+1)
+                path.pop()
+                
+        backtracking(n,k,1)
+        return fin
