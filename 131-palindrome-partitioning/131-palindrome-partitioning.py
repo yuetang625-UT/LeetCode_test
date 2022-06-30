23f5f87decb5872952da0522e24ec3dea6b529ff
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-06-30 17:30:09 -0500

    Time: 1072 ms (21.30%), Space: 32.9 MB (25.89%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        result=[]
+        path=[]
+        
+        def backtracking(s,index):
+            if index>=len(s):
+                result.append(path[:])
+                return
+            for i in range(index,len(s)):
+                temp=s[index:i+1]
+                if temp==temp[::-1]:
+                    path.append(temp)
+                    backtracking(s,i+1)
+                    path.pop()
+                else:
+                    continue
+        backtracking(s,0)
+        return result
