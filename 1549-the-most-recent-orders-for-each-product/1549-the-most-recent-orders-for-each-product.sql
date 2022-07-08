55b662a213d565d79cf78822a17e499a06ef2b56
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-07-08 14:12:09 -0500

    Time: 2791 ms (27.68%), Space: 0B (100.00%) - LeetHub

--- 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
+++ 1549-the-most-recent-orders-for-each-product/1549-the-most-recent-orders-for-each-product.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT product_name, Products.product_id, order_id, order_date FROM Products JOIN (SELECT product_id,order_id, order_date,rank() over (partition by product_id order by order_date DESC) r FROM Customers JOIN Orders USING (customer_id)) T ON T.product_id=Products.product_id
+SELECT product_name, Products.product_id, order_id, order_date FROM Products JOIN (SELECT product_id,order_id, order_date,rank() over (partition by product_id order by order_date DESC) r FROM Customers JOIN Orders USING (customer_id)) T USING(product_id)
 WHERE r=1
 ORDER BY product_name, Products.product_id, order_id
