ef9d8514bcdaaa9c989352d595e7d68e1205c85e
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-07-10 10:27:47 -0500

    Time: 900 ms (82.54%), Space: 0B (100.00%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-SELECT customer_name, customer_id, order_id, order_date FROM (SELECT name customer_name, customer_id, order_id,order_date, rank() over (partition by customer_id order by order_date DESC) r FROM Customers JOIN Orders USING(customer_id)) T WHERE r<=3 ORDER BY customer_name, customer_id, order_date DESC
+WITH T as (SELECT name customer_name, customer_id, order_id,order_date, rank() over (partition by customer_id order by order_date DESC) r FROM Customers JOIN Orders USING(customer_id)) 
+SELECT customer_name, customer_id, order_id, order_date FROM T WHERE r<=3 ORDER BY customer_name, customer_id, order_date DESC
