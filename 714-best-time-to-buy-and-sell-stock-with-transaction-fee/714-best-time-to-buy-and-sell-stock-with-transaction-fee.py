1469b1da473ebed1bebfb341249295546604c159
Yue Liu <54342920+yuetang625-UT@users.noreply.github.com>
2022-08-05 17:09:04 -0500

    Time: 1028 ms (71.97%), Space: 23.2 MB (33.24%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,18 +1,9 @@
-class Solution(object):
-    def maxProfit(self, prices, fee):
-        """
-        :type prices: List[int]
-        :type fee: int
-        :rtype: int
-        """
-        res=0
-        minp=prices[0]
-        for i in range(1,len(prices)):
-            if prices[i]<minp:
-                minp=prices[i]
-            elif prices[i]>=minp and prices[i]<=minp+fee:
-                continue
-            else:
-                res+=prices[i]-(minp+fee)
-                minp=prices[i] -fee
-        return res
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n=len(prices)
+        dp=[[0] * 2 for _ in range(n)]
+        dp[0][0]=-prices[0]
+        for i in range(1,n):
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]-prices[i])
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]+prices[i]-fee)
+        return max(dp[-1][0],dp[-1][1])
